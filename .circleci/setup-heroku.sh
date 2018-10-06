27b5ad4587ba87879edabe8d899df971706aed9f
hogetaro <hoge@hoge.com>
2018-10-06 14:07:55 +0900

    Add:circleCI

---
+++ .circleci/setup-heroku.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# TODO config.ymlの方の環境変数で設定出来るようにする。
+git remote add heroku git@heroku.com:hotels-search-react-app.git
+
+cat >> ~/.ssh/config << EOF
+Host heroku.com
+    HostName heroku.com
+    User git
+    IdentityFile ~/.ssh/heroku
+EOF
+
+chmod 600 ~/.ssh/config
+
+
+# Add heroku.com to the list of known hosts
+ssh-keyscan -H heroku.com >> ~/.ssh/known_hosts
