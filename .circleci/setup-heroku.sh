44d025dde79400d581f582d199399a3d8072b9ed
hogetaro <hoge@hoge.com>
2018-10-06 17:57:36 +0900

    Fix

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -3,11 +3,15 @@
 # TODO config.ymlの方の環境変数で設定出来るようにする。
 git remote add heroku git@heroku.com:search-hotels-app.git
 
+echo yes | heroku keys:add 
+grunt build
+yes | grunt buildcontrol:heroku 
+
 cat >> ~/.ssh/config << EOF
 Host heroku.com
     HostName heroku.com
     User git
-    IdentityFile ~/.ssh/id_rsa
+    IdentityFile ~/.ssh/heroku
 EOF
 
 chmod 600 ~/.ssh/config
