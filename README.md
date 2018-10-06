aff0af23c1364bbab7f80d84efd7b351326297de
hogetaro <hoge@hoge.com>
2018-10-06 15:36:31 +0900

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-# hotels search react app
-
-Google API と楽天 API を連携して
-検索した住所の周囲にあるホテルを検索するアプリです．
---
+++ precache-manifest.241a81e1cf04f724871ebb9f6d9bcd99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "e067026d341dcdc8d4e4",
+    "url": "./static/js/main.e067026d.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "./static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "e067026d341dcdc8d4e4",
+    "url": "./static/css/main.a5c28d55.chunk.css"
+  },
+  {
+    "revision": "75dfbc38572afc9c89b564a259e098a9",
+    "url": "./index.html"
+  }
+];
