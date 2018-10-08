3f99fe66a8803c4d60d0f1fc235df477ae3aca3c
hogetaro <hoge@hoge.com>
2018-10-09 02:09:48 +0900

    Add:post-css-variables

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   plugins: [
-    require("postcss-import")(),
+    require("postcss-import"),
+    require("postcss-simple-vars")({
+      variables: require("./src/styles/variables.css.js")
+    }),
     require("postcss-nested"),
     require("autoprefixer"),
     require("cssnano")({
