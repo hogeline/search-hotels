337cae054c6ca58c8ed5c1569f01d7cc99d78e5b
hogetaro <hoge@hoge.com>
2018-10-09 15:47:40 +0900

    Add:Geocode with Google Map API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,60 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 import SearchForm from "./SearchForm";
 import GeocodeResult from "./GeocodeResult";
+require("dotenv").config();
+
+const GEOCODE_ENDPOINT = "https://maps.googleapis.com/maps/api/geocode/json";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
+
+  setErrorMessage(message) {
+    this.setState({
+      address: message,
+      lat: 0,
+      lng: 0
+    });
+  }
+
   handlePlaceSubmit(place) {
-    console.log(place);
+    axios
+      .get(GEOCODE_ENDPOINT, {
+        params: {
+          key: process.env.REACT_APP_GOOGLEMAP_API_KEY,
+          address: place
+        }
+      })
+      .then(results => {
+        console.log(results);
+        const data = results.data;
+        const result = results.data.results[0];
+        switch (data.status) {
+          case "OK": {
+            const location = result.geometry.location;
+            this.setState({
+              address: result.formatted_address,
+              lat: location.lat,
+              lng: location.lng
+            });
+            break;
+          }
+          case "ZERO_RESULTS": {
+            this.setErrorMessage("結果が見つかりませんでした");
+            break;
+          }
+          default: {
+            this.setErrorMessage("エラーが発生しました");
+          }
+        }
+      })
+      .catch(error => {
+        this.setErrorMessage("通信に失敗しました");
+      });
   }
 
   render() {
