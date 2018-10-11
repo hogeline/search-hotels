732aa8f88262731bc6977bad1b476a172878f25d
hogetaro <hoge@hoge.com>
2018-10-12 02:32:16 +0900

    Add: Hotels column

--- src/components/HotelRow.jsx
+++ src/components/HotelRow.jsx
@@ -4,15 +4,27 @@ import PropTypes from "prop-types";
 const HotelRow = ({ hotel }) => (
   <tr>
     <td>
+      <img src={hotel.thumbUrl} alt={hotel.name} />
+    </td>
+    <td>
       <a href={hotel.url}>{hotel.name}</a>
     </td>
+    <td>{hotel.price}</td>
+    <td>{hotel.reviewAverage}</td>
+    <td>{hotel.reviewCount}</td>
+    <td>{hotel.distance}</td>
   </tr>
 );
 
 HotelRow.propTypes = {
   hotel: PropTypes.shape({
     name: PropTypes.string,
-    url: PropTypes.string
+    url: PropTypes.string,
+    thumbUrl: PropTypes.string,
+    place: PropTypes.string,
+    reviewAverage: PropTypes.string,
+    reviewCount: PropTypes.number,
+    distance: PropTypes.number
   }).isRequired
 };
 
