75432787b351a7cae9dc06774b51e07ab125c658
hogetaro <hoge@hoge.com>
2018-10-12 00:47:17 +0900

    Update:Map リファクタリング

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -5,54 +5,41 @@ import ReactGoogleMapLoader from "react-google-maps-loader";
 import { withGoogleMap, GoogleMap, Marker } from "react-google-maps";
 require("dotenv").config();
 
-const InnerMap = withGoogleMap(props => (
-  <GoogleMap
-    defaultZoom={12}
-    defaultCenter={props.position}
-    center={props.position}
-  >
-    <Marker {...props.marker} />
+const InnerMap = withGoogleMap(({ location, marker }) => (
+  <GoogleMap defaultZoom={12} defaultCenter={location} center={location}>
+    <Marker {...marker} />
   </GoogleMap>
 ));
 
-const Map = ({ lat, lng }) => {
-  const position = { lat, lng };
-  return (
-    <ReactGoogleMapLoader
-      params={{
-        key: process.env.REACT_APP_GOOGLEMAP_API_KEY,
-        libraries: "places,geometry"
-      }}
-      render={(googleMaps, error) =>
-        googleMaps ? (
-          <div>
-            <InnerMap
-              containerElement={<div />}
-              mapElement={<div className="map" />}
-              position={position}
-              marker={{ position }}
-            />
-            {error ? error : "Google Maps is loaded !"}
-          </div>
-        ) : (
-          <div>
-            {/*Check for network error so loading state ends if user lost connection.*/}
-            {error === "Network Error" ? <p>{error}</p> : <p>isLoading...</p>}
-          </div>
-        )
-      }
-    />
-  );
-};
+const Map = ({ location }) => (
+  <ReactGoogleMapLoader
+    params={{
+      key: process.env.REACT_APP_GOOGLEMAP_API_KEY,
+      libraries: "places,geometry"
+    }}
+    render={(googleMaps, error) =>
+      googleMaps ? (
+        <div>
+          <InnerMap
+            containerElement={<div />}
+            mapElement={<div className="map" />}
+            location={location}
+            marker={{ position: location }}
+          />
+          {error ? error : "Google Maps is loaded !"}
+        </div>
+      ) : (
+        <div>
+          {/*Check for network error so loading state ends if user lost connection.*/}
+          {error === "Network Error" ? <p>{error}</p> : <p>isLoading...</p>}
+        </div>
+      )
+    }
+  />
+);
 
 Map.propTypes = {
-  lat: PropTypes.number,
-  lng: PropTypes.number
-};
-
-Map.defaultProps = {
-  lat: 35.6585805,
-  lng: 139.7454329
+  location: PropTypes.objectOf(PropTypes.number.isRequired)
 };
 
 export default Map;
---
+++ src/domain/Geocoder.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const GEOCODE_ENDPOINT = "https://maps.googleapis.com/maps/api/geocode/json";
+
+export const geocode = place =>
+  axios
+    .get(GEOCODE_ENDPOINT, {
+      params: {
+        key: process.env.REACT_APP_GOOGLEMAP_API_KEY,
+        address: place
+      }
+    })
+    .then(results => {
+      const data = results.data;
+      const status = data.status;
+      const result = data.results[0];
+      if (typeof result === "undefined") {
+        return { status };
+      }
+      const address = result.formatted_address;
+      const location = result.geometry.location;
+      return { status, address, location };
+    });
